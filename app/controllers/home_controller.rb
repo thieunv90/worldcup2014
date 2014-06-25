78a8a5c74f60a929af744a072172cb837bc097f2
vanthieuuit <vanthieuuit@S.E.C.R.E.T>
2014-06-25 07:47:38 +0700

    Fix bugs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,8 @@ class HomeController < ApplicationController
     # end
     # @upcoming_games = Game.where( 'play_at > ?', Time.now ).order( 'play_at')
     @past_games = Game.where(locked: true).order( 'play_at, pos')
-    first_game_not_locked = Game.where(locked: false).order( 'play_at, pos').first
-    @recent_games = Game.where( 'date(play_at) = ?', first_game_not_locked.play_at.to_date).order( 'play_at, pos')
+    last_game_locked = Game.where(locked: true).order( 'play_at, pos').last
+    @recent_games = Game.where( 'date(play_at) = ?', last_game_locked.play_at.to_date).order( 'play_at, pos')
     @upcoming_games = Game.where(locked: false).where( 'play_at >= ?', Time.now ).order( 'play_at, pos')
     @total_money_for_final = Investment.all.map(&:remaining).sum
   end
