0528efa684502a4d941654db3c718f121e3c23bb
vanthieuuit <vanthieuuit@S.E.C.R.E.T>
2014-06-25 21:26:29 +0700

    Update home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
     @past_games = Game.where(locked: true).order( 'play_at, pos')
     last_game_locked = Game.where(locked: true).order( 'play_at, pos').last
     @recent_games = Game.where( 'date(play_at) = ?', last_game_locked.play_at.to_date).order( 'play_at, pos')
-    @upcoming_games = Game.where(locked: false).where( 'play_at >= ?', Time.now ).order( 'play_at, pos')
+    @upcoming_games = Game.where(locked: false).order( 'play_at, pos')
     @total_money_for_final = Investment.all.map(&:remaining).sum
   end
 end
